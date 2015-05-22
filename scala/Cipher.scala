1b5eef73a7755a7c4ee06463a7883ebd0f55c40f
zw <wzhao1984@gmail.com>
2015-05-22 21:39:53 +0800

    scala code for AES/DES3

--- scala/Cipher.scala
+++ scala/Cipher.scala
@@ -1,45 +1,68 @@
-import javax.crypto.Cipher
+import javax.crypto.{Cipher => jCipher}
 import javax.crypto.spec.SecretKeySpec
 import javax.crypto.spec.IvParameterSpec
 
 import javax.crypto.KeyGenerator
 import java.security.SecureRandom;
 
+class PKCS7(paddingLen: Int=8) {
+  def encode(src: Array[Byte]): Array[Byte] = {
+    val padding = paddingLen - (b.length % paddingLen)
+    src ++ Array.fill(padding)(padding)
+  }
 
-def getCipher(mode: String) = {
-  val cipher = Cipher getInstance "AES/CBC/NoPadding"
-  val skey = new SecretKeySpec(key, "AES")
-  val m = if (mode=="encrypt") Cipher.ENCRYPT_MODE else Cipher.DECRYPT_MODE
-  cipher.init(m, skey, new IvParameterSpec(iv))
-  cipher
+  def decode(src: Array[Byte]): Array[Byte] = {
+    src take (src.length - src.last)
+  }
 }
 
-def encrypt(s: String): Array[Byte] = {
-  val cipher = Cipher getInstance "AES/CBC/NoPadding"
-  val skey = new SecretKeySpec(getRawKey(key), "AES")
-  cipher.init(Cipher.ENCRYPT_MODE, skey, new IvParameterSpec(iv))
-  cipher.doFinal(s getBytes "UTF8")
-}
+class Cipher(algo: String, key: Array[Byte], iv: Array[Byte], mode: String="cbc") {
+  var mEncrypter: JCipher = null
+  var mDecrypter: JCipher = null
+  var mPadder: PKCS7 = null
 
-def encrypt1(s: String): Array[Byte] = {
-  val cipher = Cipher getInstance "AES/CBC/NoPadding"
-  val skey = new SecretKeySpec(key, "AES")
-  cipher.init(Cipher.ENCRYPT_MODE, skey, new IvParameterSpec(iv))
-  cipher.doFinal(s getBytes "UTF8")
-}
+  def encrypt(b: Array[Byte], padding: Boolean=True, end: Boolean=True): Array[Byte] = {
+    if (mEncrypter == null)
+      mEncrypter = getCipher("encrypt", padding)
+    var src = b
+    if (padding)
+      src = padder encode src
+    if (end)
+      mEncrypter.doFinal(src)
+    else
+      mEncrypter.update(src)
+  }
 
-def decrypt(bs: Array[Byte]): Array[Byte] = {
-  val cipher = Cipher getInstance "AES/CBC/NoPadding"
-  val skey = new SecretKeySpec(key, "AES")
-  cipher.init(Cipher.DECRYPT_MODE, skey, new IvParameterSpec(iv))
-  cipher.doFinal(bs)
-}
+  def decrypt(b: Array[Byte], padding: Boolean=True, end: Boolean=True): Array[Byte] = {
+    if (mDecrypter == null)
+      mDecrypter = getCipher("decrypt", padding)
+    val encrypted = 
+      if (end)
+        mEncrypter.doFinal(b)
+      else
+        mEncrypter.update(b)
+    if (padding)
+      padder decode encrypted
+    else
+      encrypted
+  }
 
-def getRawKey(key: Array[Byte]): Array[Byte] = {
-  val sr = SecureRandom.getInstance("SHA1PRNG")
-  sr.setSeed(key)
-  val kg = KeyGenerator.getInstance("AES")
-  kg.init(128, sr)
-  val k = kg.generateKey
-  k.getEncoded
+  def getCipher(opMode: String, padding: Boolean): JCipher = {
+    val initStr = "%s/%s/NoPadding".format(algo, mode).toUpperCase
+    val cipher = JCipher getInstance initStr
+    val secretKey = new SecretKeySpec(key, algo)
+    val cipherMode = opMode match {
+      case "encrypt" => JCipher.ENCRYPT_MODE
+      case "decrypt" => JCipher.DECRYPT_MODE
+      case _ => throw new IllegalArgumentException("Unknown mode: " + opMode)
+    }
+    cipher.init(cipherMode, secretKey, new IvParameterSpec(iv))
+    if (padding)
+      padder = new PKCS7(algo match {
+        case "aes" => 16
+        case "des3" => 8
+        case _ => 8
+      })
+    cipher
+  }
 }
